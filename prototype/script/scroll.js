6712b181ae45424a41f09880f3d92dbbe078577b
Ryan Transfiguracion <rtransfig@gmail.com>
2016-06-13 11:21:12 -0700

    gh-page -- debug info

--- prototype/script/scroll.js
+++ prototype/script/scroll.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(event)
+//document.addEventListener("DOMContentLoaded", function(event)
+$(window).load(function()
 {
   measureElements($(window));
 });
