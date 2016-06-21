cae68469db1e5d5b108523d541ed024e3563d8e5
Ryan Transfiguracion <rtransfig@gmail.com>
2016-06-21 15:18:22 -0700

    Added a debug widget (mostly for mobile device use). Changed the content for the services section per Clark's request. Changed the content for the about section per Clark's request. Re-arranged the Team section per Clark's request.

--- prototype/script/main.js
+++ prototype/script/main.js
@@ -1,3 +1,22 @@
+var DEBUG = true;
+var debug_line = 1;
+
+function debugPrint(str)
+{
+  if (DEBUG)
+  {
+    let msg = `${debug_line++}: ${str}`;
+    $('#debug-info').append($('<p>').append(msg));
+    console.log(msg);
+
+    $('#debug-info').animate(
+    {
+      scrollTop: '' + $('#debug-info').prop('scrollHeight') + 'px'
+    }, 0
+    );
+  }  
+}
+
 $(document).ready(function()
 {
 
