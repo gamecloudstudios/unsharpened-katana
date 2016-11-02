da193fea6349ff5565caa3ccddead91ac60cbe7c
Ryan Transfiguracion <rtransfig@gmail.com>
2016-11-02 15:32:45 -0700

    Added Google Analytics Measurement Protocol events

---
+++ prototype/script/gamp.js
@@ -0,0 +1,24 @@
+var gamp = function(event_label)
+{
+  $.ajax({
+    method: 'POST',
+    url: 'www.google-analytics.com/collect',
+    data: {
+      v: '1',
+      tid: 'UA-86681201-1',
+      aip: '1',
+      cid: '111',
+      ds: 'web',
+      t: 'pageview',
+      el: event_label
+    },
+    success: function()
+    {
+      console.log(`Request for event ${event_label} succeeded!`);
+    },
+    error: function()
+    {
+      console.log(`Request for event ${event_label} failed!`);
+    }
+  });
+};
